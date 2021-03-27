cdff93ce5ab682ee9379affad1caef5691ab17a0
soubinkittiphanh <kittiphanhsoubin@gmail.com>
2021-03-27 14:53:57 +0700

    config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-const apiDomain="http://192.168.42.49:8080/";
-// const apiDomain="https://jfillapiapp.herokuapp.com/";
+// const apiDomain="http://192.168.42.49:8080/";
+const apiDomain="https://jfillapiapp.herokuapp.com/";
 module.exports={
     url:apiDomain,
 }
