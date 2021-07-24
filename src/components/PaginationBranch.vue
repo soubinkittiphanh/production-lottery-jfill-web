114097921bfcb551e86844aff2909c3f039fd385
soubin <kittiphanhsoubin@gmail.com>
2021-07-25 00:32:20 +0700

    ud

---
+++ src/components/PaginationBranch.vue
@@ -0,0 +1,139 @@
+<template>
+  <div class="">
+    <div class="center">
+      <i class="fa fa-spinner fa-spin fa-3x fa-fw" v-if="isloading"></i>
+      <p v-else-if="!isloading && error" style="color: red">{{ error }}</p>
+    </div>
+    <div class="alert alert-success">ສະມາຊິກທັງຫມົດ </div>
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">ໂຕຫຍໍ້</th>
+          <th scope="col">ຊື່</th>
+          <th scope="col">ຂໍ້ມູນອື່ນໆ</th>
+          <th scope="col">ແກ້ໄຂ</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>ສາຂາທັງໝົດ:</td>
+          <td></td>
+          <td></td>
+          <td>{{data==null?0:data.length}}</td>
+          <td>ສາຂາ</td>
+        </tr>
+        <tr v-for="d in data" v-bind:key="d.id">
+          <td>{{ d.id }}</td>
+          <td>{{ d.code }}</td>
+          <td>{{ d.name }}</td>
+          <td>{{ d.lname }}</td>
+          <td>
+            <button class="btn btn-warning" @click="viewUser(d.id)">
+              <span class="material-icons">edit</span>
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <VueTailwindPagination
+      :current="currentPage"
+      :total="total"
+      :per-page="perPage"
+      @page-changed="onPageClick($event)"
+    />
+  </div>
+</template>
+<script>
+import "@ocrv/vue-tailwind-pagination/dist/style.css";
+import VueTailwindPagination from "@ocrv/vue-tailwind-pagination";
+import apiDomain from "../config";
+import axios from "axios";
+export default {
+  props: ["datas"],
+  components: {
+    VueTailwindPagination,
+  },
+  data() {
+    return {
+      totalsale:0,
+      totalcom3:0,
+      totalwin:0,
+      totalcom5:0,
+      totalreturn:0,
+      currentPage: 1,
+      perPage: 2,
+      total: 0,
+      data: this.datas,
+      originData: [],
+      isloading:false,
+      error:null,
+    };
+  },
+  methods: {
+    formatNum(val) {
+      return new Intl.NumberFormat().format(val);
+    },
+    viewUser(id) {
+        const sel_brc= this.data.find(data=>data.id===id);
+        console.log(":::::::::::::"+sel_brc.lname);
+      this.$emit("update-branch", id,sel_brc.code,sel_brc.name,sel_brc.lname);
+    },
+    onPageClick(event) {
+      this.currentPage = event;
+      this.getData(this.currentPage);
+    },
+    async getData(pageNumber) {
+      let responseData = [];
+      const pl = this.perPage;
+      for (let i = 0; i < pl; i++) {
+        let id = pageNumber != 1 ? pageNumber * pl - pl + i : i;
+        if (id === this.total - 1) i = pl + 1;
+        responseData.push({
+          id: this.originData[id].id,
+          code: this.originData[id].code,
+          name: this.originData[id].name,
+          lname: this.originData[id].lname,
+        });
+      }
+      this.data = responseData;
+    },
+    fetchbrc() {
+      this.isloading = true;
+      this.error = null;
+      axios
+        .get(apiDomain.url + "fetchbrc")
+        .then((res) => {
+          var results = [];
+          for (const id in res.data) {
+            results.push({
+              id: res.data[id].id,
+              code: res.data[id].co_code,
+              name: res.data[id].co_name,
+              lname: res.data[id].co_gname,
+            });
+
+          }
+          this.users = results;
+          this.data = results;
+          this.originData = this.data;
+          this.total = this.originData.length;
+          this.perPage = 10;
+          this.getData(this.currentPage);
+          this.isloading = false;
+          console.log(":::::::::::::::"+this.data.length);
+        })
+        .catch((er) => {
+          this.isloading = false;
+          this.error = er;
+          alert("ເກີດຂໍ້ຜິດພາດການເຊື່ອມຕໍ່ເຊີເວີ: " + er);
+        });
+    },
+  },
+  mounted() {
+    console.log("mounted: " + this.data);
+    this.currentPage = 1;
+    this.fetchbrc();
+  },
+};
+</script>
---
+++ src/views/BranchMgr.vue
@@ -0,0 +1,176 @@
+<template>
+  <div class="container card">
+    <!-- <button class="btn btn-primary" @click="gen_ism_ref">ໃສ່ເລກງວດ</button> -->
+    <form>
+      <div class="form-group row">
+        <label for="roll_id" class="col-md-4 col-form-label">ໂຕຫຍໍ້ສາຂາ:</label>
+        <div class="col-md-12">
+          <input
+            type="text"
+            class="form-control"
+            placeholder="::: PXK :::"
+            v-model="abbr"
+            :disabled="id"
+          />
+        </div>
+        <label for="roll_id" class="col-md-4 col-form-label">ຊື່ສາຂາ:</label>
+        <div class="col-md-12">
+          <input
+            type="text"
+            class="form-control"
+            placeholder="::: ສາຂາສະຫວັນນະເຂດ :::"
+            v-model="name"
+          />
+          <label for=""></label>
+        </div>
+        <label for="roll_id" class="col-md-4 col-form-label"
+          >ຂໍ້ມູນອື່ນໆ:</label
+        >
+        <div class="col-md-12">
+          <input
+            type="text"
+            class="form-control"
+            placeholder="::: ຂື້ນກັບທ່ານ :::"
+            v-model="desc"
+          />
+          <label for=""></label>
+        </div>
+        <div class="col-md-12">
+          <div class="row">
+            <div class="col-md-6">
+              <button
+                class="btn btn-success"
+                @click.prevent="!id ? createBrch() : updateBrch(id)"
+              >
+                {{ !id ? "ເພີ່ມ" : "ບັນທຶກ" }}
+              </button>
+            </div>
+            <div class="col-md-6">
+              <button
+                v-if="id"
+                class="btn btn-danger"
+                @click.prevent="resetText"
+              >
+                ຍົກເລີກ
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </form>
+    <hr />
+
+    <div>
+      <ul></ul>
+    </div>
+    <branch-page @update-branch="resdata" :datas="data"></branch-page>
+  </div>
+</template>
+<script>
+import axios from "axios";
+import apiDomain from "../config";
+import BranchPage from "../components/PaginationBranch.vue";
+export default {
+  components: {
+    BranchPage,
+  },
+  data() {
+    return {
+      isloading: false,
+      id: null,
+      error: null,
+      abbr: "",
+      name: "",
+      desc: "",
+      data: [],
+    };
+  },
+  methods: {
+    createBrch() {
+      if (!this.abbr || !this.name) {
+        // IF NO INFO PROVIDED
+        alert("ກະລຸນາໃສ່ຂໍ້ມູນໃຫ້ຄົບ");
+      } else {
+        axios
+          .post(apiDomain.url + "createbrc", {
+            id: this.id,
+            abbr: this.abbr,
+            name: this.name,
+            desc: this.desc,
+          })
+          .then((res) => {
+            this.isloading = false;
+            alert(res.data);
+            // res.status==200?this.fetchbrc:null
+            res.status==200?this.bus.$emit('fetch'):null
+          })
+          .catch((er) => {
+            alert("ເກີດປັນຫາຂັດຂ້ອງ: " + er);
+          });
+      }
+    },
+    resetText() {
+      this.id = null;
+      this.name = "";
+      this.abbr = "";
+      this.desc = "";
+    },
+    resdata(id, abbr, name, desc) {
+      console.log("DATA FROM WALL:" + id + " " + abbr + name + desc);
+      this.abbr = abbr;
+      this.name = name;
+      this.desc = desc;
+      this.id = id;
+    },
+    updateBrch(id) {
+      console.log(id);
+      if (!this.abbr || !this.name) {
+        // IF NO INFO PROVIDED
+        alert("ກະລຸນາໃສ່ຂໍ້ມູນໃຫ້ຄົບ");
+      } else {
+        axios
+          .put(apiDomain.url + "updatebrc", {
+            id: this.id,
+            abbr: this.abbr,
+            name: this.name,
+            desc: this.desc,
+          })
+          .then((res) => {
+            this.isloading = false;
+            alert(res.data);
+            // res.status == 200 ? (this.id = this.resetText()) : null; // RESET TEXTFIELD IF UPDATE SUCCEED
+               res.status==200?this.bus.$emit('fetch'):null
+            console.log("::::::::::" + res.status);
+          })
+          .catch((er) => {
+            alert("ເກີດປັນຫາຂັດຂ້ອງ: " + er);
+          });
+      }
+    },
+    fetchbrc() {
+      this.isloading = true;
+      this.error = null;
+      axios
+        .get(apiDomain.url + "fetchbrc")
+        .then((res) => {
+          var results = [];
+          for (const id in res.data) {
+            results.push({
+              id: res.data[id].id,
+              code: res.data[id].co_code,
+              name: res.data[id].co_name,
+              lname: res.data[id].co_gname,
+            });
+          }
+          this.data = results;
+          console.log(":::::::::::::::" + this.data.length);
+        })
+        .catch((er) => {
+          this.isloading = false;
+          this.error = er;
+          alert("ເກີດຂໍ້ຜິດພາດການເຊື່ອມຕໍ່ເຊີເວີ: " + er);
+        });
+    },
+  },
+};
+</script>
