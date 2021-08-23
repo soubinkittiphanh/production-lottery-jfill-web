7f4ee750c3977588d758135acbf168c630716332
soubinkittiphanh <kittiphanhsoubin@gmail.com>
2021-08-23 21:28:57 +0700

    host cluster

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 // const apiDomain="https://jfillapiapp.herokuapp.com/";
-const apiDomain="https://apiwebpakse.herokuapp.com/";
+// const apiDomain="https://apiwebpakse.herokuapp.com/";
+const apiDomain="https://nodejsclusters-46575-0.cloudclusters.net/";
 // const apiDomain="https://nodejsclusters-40083-0.cloudclusters.net/";
 // const apiDomain="http://localhost:8080/";//localhost
 module.exports={
