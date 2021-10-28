a6559adc8f840570fcc4cadabd16764d2566b598
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2021-10-28 12:33:37 +0700

    show all branch neck1

--- src/config.js
+++ src/config.js
@@ -1,8 +1,7 @@
 // const apiDomain="https://jfillapiapp.herokuapp.com/";
 // const apiDomain="https://apiwebpakse.herokuapp.com/";
-// const apiDomain="https://nodejsclusters-40083-0.cloudclusters.net/";
-// const apiDomain="https://nodejsclusters-40083-0.cloudclusters.net/";
-const apiDomain="http://localhost:8080/";//localhost
+const apiDomain="https://nodejsclusters-40083-0.cloudclusters.net/";
+// const apiDomain="http://localhost:8080/";//localhost
 module.exports={
     url:apiDomain,
 }
